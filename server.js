ac6ee0f72239289c893c2a755aa3b862c46fc3a1
Ernesto Vera Celiz <evcvera@gmail.com>
2021-10-14 19:09:56 -0300

    heroku

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const express = require("express");
 
 const app = express();
 
-app.use(express.static(__dirname+'/dist/cosas-de-tiempo'));
+app.use(express.static(__dirname+'/dist/POML'));
 app.get('/',function(req,res){
-  res.sendFile(path.join(__dirname+'/dist/cosas-de-tiempo/index.html'));
+  res.sendFile(path.join(__dirname+'/dist/POML/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
