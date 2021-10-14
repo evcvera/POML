60696cd6f267758a1068098a6d6d5124e931193b
Ernesto Vera Celiz <evcvera@gmail.com>
2021-10-14 18:48:32 -0300

    heroku

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
-const express = require("express");
-
+const path = require('path');
+const express = require('express');
 const app = express();
 
-app.use(express.static("./dist/cosas-de-tiempo"));
+// Serve static files
+app.use(express.static(__dirname + '/dist/cosas-de-tiempo'));
 
-app.get("/*", function (req, res) {
-  res.sendFile("index.html", { root: "dist/cosas-de-tiempo" });
+// Send all requests to index.html
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/cosas-de-tiempo/index.html'));
 });
 
-app.listen(process.env.PORT || 8080);
-
-console.log(`Running on port ${process.env.PORT || 8080}`);
+// default Heroku port
+app.listen(process.env.PORT || 5000);
