08fb9f395338b0655d8a87ba195693714793f894
Ernesto Vera Celiz <evcvera@gmail.com>
2022-03-06 00:38:33 -0300

    whitout spec

--- src/app/core/pipes/thousands-pipe.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ThousandsPipePipe } from './thousands-pipe.pipe';
-
-describe('ThousandsPipePipe', () => {
-  it('create an instance', () => {
-    const pipe = new ThousandsPipePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
